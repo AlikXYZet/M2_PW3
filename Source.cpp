ff15d24e69c82b8ed2806bbb51a3ebd76d47cb26
Александр Осипов <alikxyzet@gmail.com>
2024-06-13 04:06:21 +0300

    3.1 Adding a vector V1

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 
 int main()
 {
+	// Инициализация вектора с последующей записью и воводом
+	std::vector<int> V1;
+
+	std::cout << "{ ";
+	for (int i = 0; i < 10; i++)
+	{
+		V1.push_back(i);
+		std::cout << V1[i];
+		if (i != 9)
+			std::cout << ", ";
+	}
+	std::cout << " }" << std::endl;
 
 
 
