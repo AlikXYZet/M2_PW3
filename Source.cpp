4dc878bd6aab2bd564f594cb281cedb230572aa6
Александр Осипов <alikxyzet@gmail.com>
2024-06-13 08:32:31 +0300

    3.3 Implementing your own "const forward_iterator"

--- Source.cpp
+++ Source.cpp
@@ -15,6 +15,59 @@
 * такие операции, как p+=i, p-=i, p-p1.
 */
 
+// Реализация своего Константного forward-итератора 
+class cf_it
+{
+private:
+	std::vector<int>* V;
+	int index = 0;
+
+public:
+	cf_it(std::vector<int>& inV)
+	{
+		V = &inV;
+		index = 0;
+	}
+
+	// Чтение
+	int operator*() const
+	{
+		// Вывод ошибки, аналогичной при *V.end()
+		_STL_VERIFY(index < V->size(), "(cf_it) can't dereference out of range vector iterator");
+
+		return *(&(V->front()) + index);
+	};
+
+	// Пре-инкремент
+	cf_it& operator++()
+	{
+		if (index < (V->size()))
+			index++;
+		return *this;
+	}
+
+	// Пост-инкремент
+	cf_it operator++(int)
+	{
+		auto out = *this;
+		++(*this);
+		return out;
+	}
+
+	// Сравнение на равенство
+	bool operator==(const int* inRef) const
+	{
+		return inRef == (&(V->front()) + index);
+	};
+
+	// Сравнение на НЕравенство
+	bool operator!=(const int* inRef) const
+	{
+		return inRef != (&(V->front()) + index);
+	};
+};
+
+
 int main()
 {
 	// Инициализация вектора с последующей записью и воводом
@@ -36,7 +89,7 @@ int main()
 	std::vector<int>::const_iterator itV1;
 
 	std::cout << "{";
-	for (itV1 = V1.begin(); itV1 != V1.end(); itV1++)
+	for (itV1 = V1.begin(); itV1 != V1.end(); ++itV1)
 	{
 		std::cout << " " << *itV1 << " ";
 	}
@@ -56,6 +109,15 @@ int main()
 	std::cout << "}" << std::endl;
 	// ----------------------------------------------------------------------
 
+
+	// Использование своего итератора cf_it без использования итераторов std::vector
+	std::cout << "{";
+	for (cf_it itV2(V1); itV2 != (&V1.back() + 1); ++itV2)
+	{
+		std::cout << " " << *itV2 << " ";
+	}
+	std::cout << "}" << std::endl;
+	// ----------------------------------------------------------------------
 
 	return 0;
 }
